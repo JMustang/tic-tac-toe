3b98f6a2657cc56fe73a45ac61d8ef15b7b92887
Junior <eecfredes@gmail.com>
2023-03-26 19:24:21 -0300

    :rocket:object - player | style

--- src/object/Player/index.js
+++ src/object/Player/index.js
@@ -1,5 +1,11 @@
 import React from "react";
+import "./style.css";
+import playerX from "../../image/X.png";
 
-const Player = () => <button className="player">x</button>;
+const Player = () => (
+  <button className="player">
+    <img src={playerX} className="playerX" alt="player X" />
+  </button>
+);
 
 export default Player;
