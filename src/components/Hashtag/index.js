a0543ebcb3cb022372e0e819cc2bf2fac265009b
Junior <eecfredes@gmail.com>
2023-03-28 18:32:21 -0300

    :rocket:component - grid structure

--- src/components/Hashtag/index.js
+++ src/components/Hashtag/index.js
@@ -1,11 +1,41 @@
 import React from "react";
+import "./style.css";
 import Card from "../../object/Card";
 import Player from "../../object/Player";
 
 const Hashtag = () => (
   <Card>
-    <Player player="o" />
-    <Player player="x" />
+    <ul className="hashtag">
+      <li className="item">
+        <Player player="o" />
+      </li>
+      <li className="item">
+        <Player player="x" />
+      </li>
+      <li className="item">
+        <Player player="x" />
+      </li>
+
+      <li className="item">
+        <Player player="o" />
+      </li>
+      <li className="item">
+        <Player player="x" />
+      </li>
+      <li className="item">
+        <Player player="o" />
+      </li>
+
+      <li className="item">
+        <Player player="x" />
+      </li>
+      <li className="item">
+        <Player player="o" />
+      </li>
+      <li className="item">
+        <Player player="x" />
+      </li>
+    </ul>
   </Card>
 );
 
