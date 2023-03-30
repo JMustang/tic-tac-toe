87eb17b3cdb68bf0fee4316e7d690f77bc60f66e
Junior <eecfredes@gmail.com>
2023-03-30 16:50:53 -0300

    :rocket: objects - Associate the Label to the Input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
 
-import Label from "./object/Label";
+import Input from "./object/Input";
 import Header from "./components/header";
 import Hashtag from "./components/Hashtag";
-import Input from "./object/Input";
 
 const App = () => (
   <main className="app">
     <Header />
     <Hashtag />
-    <Input value="show" />
-    <Label content="Show events" />
+    <Input id="show" value="show" content="Shows event" />
   </main>
 );
 
