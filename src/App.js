c6c070606ecebb70665b8fd685062724877ad783
Junior <eecfredes@gmail.com>
2023-03-30 16:38:23 -0300

    :rocket: objects - input

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Label from "./object/Label";
 import Header from "./components/header";
 import Hashtag from "./components/Hashtag";
+import Input from "./object/Input";
 
 const App = () => (
   <main className="app">
     <Header />
     <Hashtag />
+    <Input value="show" />
     <Label content="Show events" />
   </main>
 );
